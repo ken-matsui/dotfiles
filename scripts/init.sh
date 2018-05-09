713b0ed08b37c0905c38a9952c714a233e79dc4e
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2018-05-09 21:16:04 +0900

    Improve

--- scripts/init.sh
+++ scripts/init.sh
@@ -39,14 +39,7 @@ git config --global user.name $gitname
 git config --global user.email $gitemail
 
 # Install anyenv and *env
-git clone -q https://github.com/riywo/anyenv ~/.anyenv
-export PATH="$HOME/.anyenv/bin:$PATH"
-eval "$(anyenv init - --no-rehash)"
-git clone -q https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
-ENVDIR="${DOTSPATH}/scripts/anyenv"
-for file in $( ls $ENVDIR ); do
-	bash ${ENVDIR}/${file}
-done
+bash "${DOTSPATH}/scripts/anyenv/main.sh"
 
 # Install ansible (and accompany some of it)
 brew install ansible 1>/dev/null

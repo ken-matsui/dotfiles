a148c7a938dc14c2b7887d1176447ae5450ffc4e
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2018-05-11 00:55:42 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -e
 
 cd ~/
 ARCHIVE="https://github.com/matken11235/dotfiles/archive/master.zip"
+trap 'rm -f master.zip' 0
 curl -fsSLO $ARCHIVE
 unzip -q ${ARCHIVE##*/}
 mv $(echo ${ARCHIVE%.*} | awk -F '/' '{printf $5"-"$7}') dotfiles

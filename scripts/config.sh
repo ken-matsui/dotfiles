2251ffe3e7dd9f9f9898d17bdac96d14d24c802e
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2018-05-12 17:37:31 +0900

    Update config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -12,7 +12,7 @@ git clone -q https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # Install Ricty with Powerline
 brew install sanemat/font/ricty --with-powerline 1>/dev/null
 cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/
-fc-cache -vf
+fc-cache -f 1>/dev/null
 
 # Link setting files.
 for conf in $(find ${DOTSPATH}/config -type f | grep -v 'com.googlecode.iterm2.plist'); do

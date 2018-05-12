0f7a25079bb72fae84126aed611a6739b5f97d1a
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2018-05-13 06:55:26 +0900

    Improve

--- scripts/dmgs/qfinder.sh
+++ scripts/dmgs/qfinder.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-url='https://eu1.qnap.com/Storage/Utility/QNAPQfinderProMac-2.6.0.0214.dmg'
-filename=${url##*/}
+#url='https://eu1.qnap.com/Storage/Utility/QNAPQfinderProMac-2.6.0.0214.dmg'
+#filename=${url##*/}
 
-curl -fsLO $url
-mount_dir=`hdiutil attach $filename | awk -F '\t' 'END{print $NF}'`
-yes | sudo installer -pkg "${mount_dir}/Qfinder Pro.pkg" -target / >/dev/null
-hdiutil detach "$mount_dir" >/dev/null
-rm -rf $filename
+#curl -fsLO $url
+#mount_dir=`hdiutil attach $filename | awk -F '\t' 'END{print $NF}'`
+#yes | sudo installer -pkg "${mount_dir}/Qfinder Pro.pkg" -target / >/dev/null
+#hdiutil detach "$mount_dir" >/dev/null
+#rm -rf $filename

5b03943710c95b054717997c99a9ac7bb3f8b334
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2020-05-13 01:41:20 -0700

    Delete scripts/anyenv

--- scripts/anyenv/ndenv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export PATH="$HOME/.anyenv/bin:$PATH"
-anyenv install ndenv 1>/dev/null
-eval "$(anyenv init - --no-rehash)"
-newest=$(ndenv install -l | grep -v - | tail -1)
-ndenv install $newest 1>/dev/null
-ndenv global $newest

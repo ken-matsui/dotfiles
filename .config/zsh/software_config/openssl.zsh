b92843be802fb5c0b2dfbdd1fa90fdaae3fae9df
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2021-11-09 07:29:30 +0900

    Add some fixes and cross.zsh

--- .config/zsh/software_config/openssl.zsh
+++ .config/zsh/software_config/openssl.zsh
@@ -1,5 +1,5 @@
 if (( $+commands[brew] )); then
-  export PATH="${HOMEBREW_PREFIX}/opt/openssl@3/bin:$PATH"
-  export LDFLAGS="-L${HOMEBREW_PREFIX}/opt/openssl@3/lib"
-  export CPPFLAGS="-I${HOMEBREW_PREFIX}/opt/openssl@3/include"
+  export PATH="$HOMEBREW_PREFIX/opt/openssl@3/bin:$PATH"
+  export LDFLAGS="-L$HOMEBREW_PREFIX/opt/openssl@3/lib"
+  export CPPFLAGS="-I$HOMEBREW_PREFIX/opt/openssl@3/include"
 fi
--- .config/zsh/software_config/python.zsh
+++ .config/zsh/software_config/python.zsh
@@ -1,2 +1,4 @@
-export PATH=$HOMEBREW_PREFIX/bin:$PATH
-export PATH=$HOMEBREW_PREFIX/opt/python@3.9/libexec/bin:$PATH
+if (( $+commands[brew] )); then
+  export PATH=$HOMEBREW_PREFIX/bin:$PATH
+  export PATH=$HOMEBREW_PREFIX/opt/python@3.9/libexec/bin:$PATH
+fi

a5fc4bf4480a0fb0edc4e1983851551f009f8ec1
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2021-11-29 02:28:38 +0900

    Replace diff-so-fancy with delta

--- .config/zsh/zplug.zsh
+++ .config/zsh/zplug.zsh
@@ -11,7 +11,6 @@ zplug "modules/directory", from:prezto
 zplug "modules/completion", from:prezto
 zplug "zsh-users/zsh-autosuggestions", defer:2
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
-zplug "so-fancy/diff-so-fancy", as:command
 
 # Install plugins if there are plugins that have not been installed
 if ! zplug check --verbose; then

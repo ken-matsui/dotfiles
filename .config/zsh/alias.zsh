cd1494de056d2971f40c64122748c9ab97eb0a39
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2021-11-29 01:00:07 +0900

    Update alias.zsh

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -2,6 +2,5 @@
 # Aliases
 ##################################
 
-alias gigafter='git rm --cached $(git ls-files --full-name -i --exclude-standard)' # After adding .gitignore, ignore files
 alias deletedocker='docker ps -aq | xargs docker rm && docker images -aq | xargs docker rmi'
 alias rm='trash'

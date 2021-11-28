5167041a7fdb639d969eb6f072470ab665c9a8d3
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2021-11-29 02:52:53 +0900

    Update function.zsh

--- .config/zsh/function.zsh
+++ .config/zsh/function.zsh
@@ -2,11 +2,6 @@
 # Functions
 ##################################
 
-# .gitignore generator
-function gi() {
-  curl -fsSL https://www.gitignore.io/api/$@ >>! $PWD/.gitignore && echo "added $@ to $PWD/.gitignore"
-}
-
 # Replace strings recursively (example: $ replace ./ hey hello)
 function replace() {
   find $1 -type f -print0 | xargs -0 sed -i '' -e "s/$2/$3/g"

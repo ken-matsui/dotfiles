3aca8029ba6635d3a1cac39bd9ff0233a39d7ee4
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2021-11-30 12:41:30 +0900

    bump system python version from 3.9 to 3.10

--- .config/zsh/software_config/python.zsh
+++ .config/zsh/software_config/python.zsh
@@ -1,4 +1,4 @@
 if (( $+commands[brew] )); then
   export PATH=$HOMEBREW_PREFIX/bin:$PATH
-  export PATH=$HOMEBREW_PREFIX/opt/python@3.9/libexec/bin:$PATH
+  export PATH=$HOMEBREW_PREFIX/opt/python@3.10/libexec/bin:$PATH
 fi

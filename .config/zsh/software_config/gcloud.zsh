35e1303e3cee870535db5f5603070bbc92f21b4f
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2021-12-29 04:35:26 +0900

    Update gcloud.zsh

--- .config/zsh/software_config/gcloud.zsh
+++ .config/zsh/software_config/gcloud.zsh
@@ -1,5 +1,5 @@
 if (( $+commands[brew] )); then
-  # Google Cloud SDK (gcloud)
+  # Google Cloud SDK (gcloud command)
   source $HOMEBREW_PREFIX/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
   source $HOMEBREW_PREFIX/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc
 fi

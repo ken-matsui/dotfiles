93a867e9e0cd6fe6285b0b1964ad2680c1a2ce60
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-05-21 14:28:38 +0900

    Update clang.zsh

--- .config/zsh/software_config/clang.zsh
+++ .config/zsh/software_config/clang.zsh
@@ -1,4 +1,4 @@
 if (( $+commands[brew] )); then
   alias clang-tidy=$HOMEBREW_PREFIX/opt/llvm/bin/clang-tidy
-  alias clang-format=$HOMEBREW_PREFIX/opt/llvm/bin/clang-format
+  alias clang-format=$HOMEBREW_PREFIX/opt/clang-format/bin/clang-format
 fi

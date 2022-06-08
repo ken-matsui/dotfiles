5e0cff8b13bad4b98b7188b6ccb784e88194ef02
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-06-08 10:39:55 +0900

    Update alias.zsh

--- .config/zsh/config/alias.zsh
+++ .config/zsh/config/alias.zsh
@@ -3,8 +3,8 @@
 ##################################
 
 # https://unix.stackexchange.com/a/81699/37512
-alias wanip='\dig @resolver4.opendns.com myip.opendns.com +short' # Get your public IP address
-alias wanip4='\dig @resolver4.opendns.com myip.opendns.com +short -4' # Get your public IPv4 address
-alias wanip6='\dig @resolver1.ipv6-sandbox.opendns.com AAAA myip.opendns.com +short -6' # Get your public IPv6 address
+alias myip='\dig @resolver4.opendns.com myip.opendns.com +short' # Get your public IP address
+alias myip4='\dig @resolver4.opendns.com myip.opendns.com +short -4' # Get your public IPv4 address
+alias myip6='\dig @resolver1.ipv6-sandbox.opendns.com AAAA myip.opendns.com +short -6' # Get your public IPv6 address
 
 alias deletedocker='docker ps -aq | xargs docker rm && docker images -aq | xargs docker rmi'

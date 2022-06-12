f93f63a525dca929b16465a43d90dea49f014920
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-06-12 14:28:42 +0900

    Update option.zsh

--- .config/zsh/config/option.zsh
+++ .config/zsh/config/option.zsh
@@ -4,15 +4,26 @@
 
 # Set operations.
 setopt extended_glob
-# Appends every command to the history file once it is executed
-setopt inc_append_history
-# Reloads the history whenever you use it
-setopt share_history
 # {a-d} expands to a b c d
 setopt braceccl
-# Remove all duplicates from history
-setopt histignorealldups
 # Ignore patterns such as URL
 setopt nonomatch
 # Don't highlight when selecting words
 unset zle_bracketed_paste
+
+#
+# History
+#
+export HISTSIZE=10000000
+export SAVEHIST=10000000
+setopt bang_hist                 # Treat the '!' character specially during expansion.
+setopt extended_history          # Write the history file in the ":start:elapsed;command" format.
+setopt inc_append_history        # Write to the history file immediately, not when the shell exits.
+setopt share_history             # Share history between all sessions.
+setopt hist_expire_dups_first    # Expire duplicate entries first when trimming history.
+setopt hist_ignore_dups          # Don't record an entry that was just recorded again.
+setopt hist_ignore_all_dups      # Delete old recorded entry if new entry is a duplicate.
+setopt hist_find_no_dups         # Do not display a line previously found.
+setopt hist_ignore_space         # Don't record an entry starting with a space.
+setopt hist_save_no_dups         # Don't write duplicate entries in the history file.
+setopt hist_reduce_blanks        # Remove superfluous blanks before recording entry.

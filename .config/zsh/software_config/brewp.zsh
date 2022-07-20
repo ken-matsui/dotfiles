ec12fe99c4da402d28914a4bf1de79a50150f170
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-07-20 20:04:11 +0900

    Revert "Add fig"
    This reverts commit d2842fd1d0c66964173c48ab4ed989316cfeef5f.

--- .config/zsh/software_config/brewp.zsh
+++ .config/zsh/software_config/brewp.zsh
@@ -83,7 +83,6 @@ brewp discord --cask
 brewp docker --cask
 # brewp drawio --cask
 brewp dropbox --cask
-brewp fig --cask
 # brewp figma --cask
 # brewp firefox --cask
 brewp font-jetbrains-mono-nerd-font --cask  # for alacritty

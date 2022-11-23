8208ec8b0a2617401df4a62f476be933a9ef9236
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-11-23 12:32:49 -0800

    Update clang.zsh -> llvm.zsh

--- .config/zsh/software_config/llvm.zsh
+++ .config/zsh/software_config/llvm.zsh
@@ -1,4 +1,4 @@
 if (( $+commands[brew] )); then
-  alias clang-tidy=$HOMEBREW_PREFIX/opt/llvm/bin/clang-tidy
+  export PATH="$HOMEBREW_PREFIX/opt/llvm/bin:$PATH"
   alias clang-format=$HOMEBREW_PREFIX/opt/clang-format/bin/clang-format
 fi

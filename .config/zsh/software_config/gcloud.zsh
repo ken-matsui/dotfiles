c363f0a6ab8e50d690991752a901ed9f380f3a6c
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-02-16 18:35:47 -0800

    Update gcloud.zsh

--- .config/zsh/software_config/gcloud.zsh
+++ .config/zsh/software_config/gcloud.zsh
@@ -1,4 +1,4 @@
-if (( $+commands[brew] )); then
+if (( $+commands[brew] )) && [[ "$(uname -s)" = Darwin ]]; then
   # Google Cloud SDK (gcloud command)
   source $HOMEBREW_PREFIX/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
   source $HOMEBREW_PREFIX/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc

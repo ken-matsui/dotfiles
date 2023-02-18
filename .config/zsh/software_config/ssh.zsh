860f2e5b538e4a9e31eb599d1df0484c14a461e2
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-02-17 22:19:25 -0800

    Support 1password ssh key agent for Ubuntu

--- .config/zsh/software_config/ssh.zsh
+++ .config/zsh/software_config/ssh.zsh
@@ -1,1 +1,5 @@
-export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+if [[ "$OSTYPE" == darwin* ]]; then
+    export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+elif [[ "$OSTYPE" == linux* ]]; then
+    export SSH_AUTH_SOCK=~/.1password/agent.sock
+fi

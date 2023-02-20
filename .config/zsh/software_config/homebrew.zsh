f1b5d10b4fd13c2e644353f9d6eaa855b51db002
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-02-20 08:16:44 -0800

    Run compaudit just in case we receive warnings from compinit

--- .config/zsh/software_config/homebrew.zsh
+++ .config/zsh/software_config/homebrew.zsh
@@ -3,6 +3,7 @@ if (( $+commands[brew] )); then
   FPATH=$HOMEBREW_PREFIX/share/zsh/site-functions:$FPATH
   autoload -Uz compinit
   compinit
+  compaudit # in case we receive warnings on compinit; to fix on macOS, chmod -R go-w $DIR
 
   # Local variable
   HOMEBREW_PREFIX=$(brew --prefix)

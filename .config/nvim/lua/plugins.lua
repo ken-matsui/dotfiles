9887f5cc5107aeded9c38ff8f9a8ddbc1c3175c7
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-03-24 00:07:23 -0700

    Update plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -120,19 +120,15 @@ return require('packer').startup(function(use)
     config = function()
       require('nvim-treesitter.configs').setup {
         ensure_installed = "all",
-
         highlight = {
           enable = true,
         },
-
         indent = {
           enable = true,
         },
-
         autotag = {
           enable = true,
         },
-
         rainbow = {
           enable = true,
         },

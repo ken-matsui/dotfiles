8a25a5f638ef83b484e7134f0c4418a70401d50f
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-04-20 16:07:50 -0700

    Use HiPhish/nvim-ts-rainbow2

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -5,7 +5,18 @@ return {
     event = { "BufReadPost", "BufNewFile" },
     dependencies = {
       'windwp/nvim-ts-autotag',
-      'p00f/nvim-ts-rainbow'
+      {
+        "HiPhish/nvim-ts-rainbow2",
+        config = function()
+          -- HACK: https://github.com/p00f/nvim-ts-rainbow/issues/112#issuecomment-1310835936
+          vim.api.nvim_create_autocmd({ "BufWritePost", "FocusGained" }, {
+            callback = function()
+              vim.cmd "TSDisable rainbow"
+              vim.cmd "TSEnable rainbow"
+            end,
+          })
+        end,
+      },
     },
     build = ':TSUpdate',
     opts = {

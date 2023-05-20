1156494b7a7d0b2c8b924d01475a07bc844e1a95
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-05-20 04:43:33 -0700

    Update morello.zsh

--- .config/zsh/software_config/morello.zsh
+++ .config/zsh/software_config/morello.zsh
@@ -1,5 +1,6 @@
 if [[ "$OSTYPE" == darwin* ]]; then
-  export CLANG=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang
+  # export CLANG=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang
+  export CLANG=/opt/homebrew/opt/llvm/bin/clang
 else
   export CLANG=clang
 fi

7e844e9a19523745106b57b51b96577060f0fd86
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-28 17:16:08 -0700

    Add auto-session

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -92,6 +92,16 @@ return {
   },
 
   {
+    'rmagatti/auto-session',
+    config = function()
+      require("auto-session").setup {
+        log_level = "error",
+        auto_session_suppress_dirs = {"~/", "~/Downloads", "/"},
+      }
+    end
+  },
+
+  {
     'RRethy/vim-illuminate',
     event = { "BufReadPost", "BufNewFile" },
     config = function ()

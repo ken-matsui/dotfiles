a9f8acd42b8c8a39ca8659c9522d381dffbddb8b
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-28 19:31:31 -0700

    Fix workaround for nvim-ts-rainbow2

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -10,12 +10,8 @@ return {
         "HiPhish/nvim-ts-rainbow2",
         config = function()
           -- HACK: https://github.com/p00f/nvim-ts-rainbow/issues/112#issuecomment-1310835936
-          vim.api.nvim_create_autocmd({ "BufWritePost", "FocusGained" }, {
-            callback = function()
-              vim.cmd "TSDisable rainbow"
-              vim.cmd "TSEnable rainbow"
-            end,
-          })
+          vim.cmd "autocmd BufNewFile,BufReadPost * TSDisable rainbow | TSEnable rainbow | TSDisable rainbow | TSEnable rainbow | TSDisable rainbow | TSEnable rainbow"
+          vim.cmd "autocmd BufWritePost * TSDisable rainbow | TSEnable rainbow"
         end,
       },
     },

331823cd72fcc9ddd4382c163a736c0fe35bc705
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-09-29 13:31:45 -0700

    Nvim: use prochri/telescope-all-recent.nvim instead

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -118,19 +118,20 @@ return {
           override_file_sorter = true,     -- override the file sorter
           case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
                                            -- the default case_mode is "smart_case"
-        }
+        },
+
       }
     },
   },
-
   {
-  "nvim-telescope/telescope-frecency.nvim",
-    config = function()
-      require("telescope").load_extension "frecency"
-    end,
-    dependencies = { "kkharji/sqlite.lua" },
-    keys = {
-      { '<Leader>o', '<Cmd>Telescope frecency<Cr>', 'n' },
+    'prochri/telescope-all-recent.nvim',
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
+      "kkharji/sqlite.lua"
     },
+    keys = {
+      { '<Leader>o', function() require('telescope.builtin').find_files() end, 'n' },
+    },
+    config = true,
   }
 }

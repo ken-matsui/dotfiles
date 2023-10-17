03ce6cf7956b1325301d4344e3fcd6c628727e26
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-10-16 22:19:47 -0700

    Nvim: bump ibl version

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -34,31 +34,42 @@ return {
 
   {
     "lukas-reineke/indent-blankline.nvim",
-    tag = "v2.20.8",
+    tag = "v3.3.2",
     event = { "BufReadPost", "BufNewFile" },
-    opts = {
-      filetype_exclude = {
-        "help",
-        "nvim-tree",
-        "lazy",
-      },
-      char = "",
-      char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-      },
-      space_char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-      },
-      show_trailing_blankline_indent = false,
-      show_current_context = true,
-    },
-    config = function(_, opts)
-      vim.cmd [[highlight IndentBlanklineIndent1 guibg=#1f1f1f gui=nocombine]]
-      vim.cmd [[highlight IndentBlanklineIndent2 guibg=#1a1a1a gui=nocombine]]
+    config = function()
+      local hooks = require("ibl.hooks")
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        vim.api.nvim_set_hl(0, "IBLYellow", { fg = "#E5C07B" })
+      end)
 
-      require("indent_blankline").setup(opts)
+      local highlight = {
+        "CursorColumn",
+        "Whitespace",
+      }
+
+      require("ibl").setup({
+        exclude = {
+          filetypes = {
+            "nvim-tree",
+            "lazy",
+          },
+        },
+        indent = {
+          highlight = highlight,
+          char = "▏",
+        },
+        whitespace = {
+          highlight = highlight,
+          remove_blankline_trail = false,
+        },
+        scope = {
+          enabled = true,
+          show_start = false,
+          highlight = {
+            "IBLYellow",
+          },
+        },
+      })
     end
   },
 

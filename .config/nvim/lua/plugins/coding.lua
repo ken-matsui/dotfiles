13e8c7d98671001807524be17c24ac87049e5d2c
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-10-24 18:45:43 -0700

    Nvim: add keybindings for gitsigns.nvim

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -53,6 +53,24 @@ return {
           vim.schedule(function() gs.prev_hunk() end)
           return '<Ignore>'
         end, {expr=true})
+
+        -- Actions
+        map('n', '<leader>gs', gs.stage_hunk)
+        map('n', '<leader>gr', gs.reset_hunk)
+        map('v', '<leader>gs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+        map('v', '<leader>gr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+        map('n', '<leader>gS', gs.stage_buffer)
+        map('n', '<leader>gu', gs.undo_stage_hunk)
+        map('n', '<leader>gR', gs.reset_buffer)
+        map('n', '<leader>gp', gs.preview_hunk)
+        map('n', '<leader>gb', function() gs.blame_line{full=true} end)
+        map('n', '<leader>tb', gs.toggle_current_line_blame)
+        map('n', '<leader>gd', gs.diffthis)
+        map('n', '<leader>gD', function() gs.diffthis('~') end)
+        map('n', '<leader>td', gs.toggle_deleted)
+
+        -- Text object
+        map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
       end,
     },
   },

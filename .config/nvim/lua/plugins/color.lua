f27b684a7173e53040153f8485f62357c978f0ed
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-11-20 02:44:52 -0800

    Nvim: migration to the newer treesitter

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -52,13 +52,12 @@ return {
       autotag = {
         enable = true,
       },
-      context_commentstring = {
-        enable = true,
-        enable_autocmd = false,
-      },
     },
     config = function(_, opts)
       require('nvim-treesitter.configs').setup(opts)
+      require('ts_context_commentstring').setup {
+        enable_autocmd = false,
+      }
       require('Comment').setup {
         pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
       }

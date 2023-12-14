437a69ec6d2c2ea8f4f096930a15733b996f11ed
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-12-14 00:29:48 -0800

    atuin: zsh-autosuggestions integration

--- .config/zsh/lazy/atuin.zsh
+++ .config/zsh/lazy/atuin.zsh
@@ -1,3 +1,8 @@
 if (( $+commands[atuin] )); then
   eval "$(atuin init zsh --disable-up-arrow)"
+
+  _zsh_autosuggest_strategy_atuin_top() {
+    suggestion=$(atuin search --cmd-only --limit 1 --search-mode prefix $1)
+  }
+  ZSH_AUTOSUGGEST_STRATEGY=atuin_top
 fi

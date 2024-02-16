1f122f64ed75d028e6987a408be0d01f6003ccf2
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-02-15 21:49:10 -0800

    Zsh: use path for clang-format

--- .config/zsh/lazy/llvm.zsh
+++ .config/zsh/lazy/llvm.zsh
@@ -1,4 +1,4 @@
 if (( $+commands[brew] )); then
   export PATH="$HOMEBREW_PREFIX/opt/llvm/bin:$PATH"
-  alias clang-format=$HOMEBREW_PREFIX/opt/clang-format/bin/clang-format
+  export PATH="$HOMEBREW_PREFIX/opt/clang-format/bin:$PATH"
 fi

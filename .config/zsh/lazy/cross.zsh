9ce899f9bb4e92f042fd556f27b179eab90f698d
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-02-15 21:50:07 -0800

    Zsh: remove cross

--- .config/zsh/lazy/cross.zsh
+++
@@ -1,6 +0,0 @@
-if (( $+commands[brew] )); then
-  export CC_x86_64_unknown_linux_musl="$HOMEBREW_PREFIX/bin/x86_64-unknown-linux-musl-gcc"
-  export CXX_x86_64_unknown_linux_musl="$HOMEBREW_PREFIX/bin/x86_64-unknown-linux-musl-g++"
-  export AR_x86_64_unknown_linux_musl="$HOMEBREW_PREFIX/bin/x86_64-unknown-linux-musl-ar"
-  export CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER="$CC_x86_64_unknown_linux_musl"
-fi

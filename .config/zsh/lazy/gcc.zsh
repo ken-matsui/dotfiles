db15b4e51c173dd7bc1b67119739d62571d5c4c5
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-02-20 21:39:00 -0800

    Add GCC commands

--- .config/zsh/lazy/gcc.zsh
+++ .config/zsh/lazy/gcc.zsh
@@ -7,3 +7,7 @@ alias make_release_gcc='make -j8'
 alias test_frontend_gcc='make check-gcc-c++ RUNTESTFLAGS="dg.exp=ext*"'
 alias test_frontend_ext_gcc='make check-gcc-c++ RUNTESTFLAGS="dg.exp=ext*"'
 alias test_libstdcxx_gcc='make -j8 check-target-libstdc++-v3'
+
+local CC1PLUS_CMD='./cc1plus -I../../../gcc/objdir/x86_64-pc-linux-gnu/libstdc++-v3/include/x86_64-pc-linux-gnu -I../../../gcc/objdir/x86_64-pc-linux-gnu/libstdc++-v3/include -I../../../gcc/libstdc++-v3/libsupc++ -I../x86_64-pc-linux-gnu/libstdc++-v3/include/x86_64-pc-linux-gnu -I../../../gcc/libstdc++-v3/include/backward -I../../../gcc/libstdc++-v3/testsuite/util -I../../gcc/ginclude'
+alias gcc_compile='eval $CC1PLUS_CMD'
+alias gcc_gdb='eval gdb --args $CC1PLUS_CMD'

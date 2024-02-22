1b1ed761fa5059ba491d27d9495cff17f029f8a6
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-02-22 07:16:19 -0800

    Zsh: prefix by gcc_ for gcc-related aliases

--- .config/zsh/lazy/gcc.zsh
+++ .config/zsh/lazy/gcc.zsh
@@ -1,12 +1,12 @@
-alias conf_debug_gcc='../configure --enable-languages=c++ --disable-error --disable-bootstrap --enable-checking'
-alias make_debug_gcc='make CXXFLAGS="-g3 -O0" -j8'
+alias gcc_conf_debug='../configure --enable-languages=c++ --disable-error --disable-bootstrap --enable-checking'
+alias gcc_make_debug='make CXXFLAGS="-g3 -O0" -j8'
 
-alias conf_release_gcc='../configure --enable-languages=c++ --disable-error --disable-bootstrap --enable-checking=release'
-alias make_release_gcc='make -j8'
+alias gcc_conf_release='../configure --enable-languages=c++ --disable-error --disable-bootstrap --enable-checking=release'
+alias gcc_make_release='make -j8'
 
-alias test_frontend_gcc='make check-gcc-c++ RUNTESTFLAGS="dg.exp=ext*"'
-alias test_frontend_ext_gcc='make check-gcc-c++ RUNTESTFLAGS="dg.exp=ext*"'
-alias test_libstdcxx_gcc='make -j8 check-target-libstdc++-v3'
+alias gcc_test_frontend='make check-gcc-c++ RUNTESTFLAGS="dg.exp=ext*"'
+alias gcc_test_frontend_ext='make check-gcc-c++ RUNTESTFLAGS="dg.exp=ext*"'
+alias gcc_test_libstdcxx='make -j8 check-target-libstdc++-v3'
 
 local CC1PLUS_CMD='./cc1plus -I../../../gcc/objdir/x86_64-pc-linux-gnu/libstdc++-v3/include/x86_64-pc-linux-gnu -I../../../gcc/objdir/x86_64-pc-linux-gnu/libstdc++-v3/include -I../../../gcc/libstdc++-v3/libsupc++ -I../x86_64-pc-linux-gnu/libstdc++-v3/include/x86_64-pc-linux-gnu -I../../../gcc/libstdc++-v3/include/backward -I../../../gcc/libstdc++-v3/testsuite/util -I../../gcc/ginclude'
 alias gcc_compile='eval $CC1PLUS_CMD'

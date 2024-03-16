76d1a3b2e92d50009b5061e0254f16995ac5d5a1
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-03-16 04:30:18 -0700

    Zsh: tweak gcc

--- .config/zsh/lazy/gcc.zsh
+++ .config/zsh/lazy/gcc.zsh
@@ -11,6 +11,7 @@ alias gcc_test_libstdcxx='make -j8 check-target-libstdc++-v3'
 export GCC_SRC_DIR="$HOME/Desktop/gcc"
 export GCC_OBJ_DIR="$GCC_SRC_DIR/objdir"
 export GCC_OUT_DIR="$GCC_OBJ_DIR/gcc"
+export PATH="$PATH:$GCC_OUT_DIR"
 
 local headers="-I$GCC_OBJ_DIR/x86_64-pc-linux-gnu/libstdc++-v3/include/x86_64-pc-linux-gnu -I$GCC_OBJ_DIR/x86_64-pc-linux-gnu/libstdc++-v3/include -I$GCC_SRC_DIR/libstdc++-v3/libsupc++ -I$GCC_OBJ_DIR/x86_64-pc-linux-gnu/libstdc++-v3/include/x86_64-pc-linux-gnu -I$GCC_SRC_DIR/libstdc++-v3/include/backward -I$GCC_SRC_DIR/libstdc++-v3/testsuite/util -I$GCC_SRC_DIR/gcc/ginclude"
 

f5a883f797c942d70dc7e7b6a5ed31f285852e0c
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-03-17 01:34:31 -0700

    Update rust.sh

--- setup/common/rust.sh
+++ setup/common/rust.sh
@@ -2,7 +2,4 @@
 set -eu
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y
-
-cargo install cargo-update
-cargo install cargo-cache
-cargo install suggest-command-not-found
+cargo install cargo-update cargo-cache suggest-command-not-found

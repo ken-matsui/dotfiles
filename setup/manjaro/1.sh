4c6ab6fc650aedef214a434abc08021555153e5c
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-03-17 01:01:17 -0700

    Create 1.sh

---
+++ setup/manjaro/1.sh
@@ -0,0 +1,36 @@
+sudo pacman -Syu
+sudo pacman -S \
+  python dbus-python fakeroot python-gpgme patch \
+  alacritty discord docker \
+  ttf-jetbrains-mono-nerd powerline-fonts \
+  atuin \
+  bat \
+  python-black \
+  boost \
+  cmake \
+  fd \
+  ffmpeg \
+  github-cli \
+  git-delta \
+  graphviz \
+  gzip \
+  hyperfine \
+  jq \
+  ripgrep \
+  libarchive \
+  libgit2 \
+  llvm \  # clangd, clang-format, clang-tidy
+  lsd \
+  ninja \
+  neomutt \
+  neovim \
+  nodejs \
+  python-poetry \
+  rust \
+  starship \
+  tmux \
+  ctags \
+  w3m \
+  yt-dlp
+pamac build 1password 1password-cli dropbox slack-desktop google-chrome notesnook topgrade google-java-format zoom
+cargo install cargo-update suggest-command-not-found

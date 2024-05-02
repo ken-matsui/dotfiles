be70e60821ab23f1199cfb18ee6119e5dd4050cc
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-05-02 02:16:17 -0700

    Install git-interactive-rebase-tool via Cargo

--- setup/common/cargo.sh
+++ setup/common/cargo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eu
 
-cargo install cargo-update cargo-cache suggest-command-not-found
+cargo install cargo-update cargo-cache suggest-command-not-found git-interactive-rebase-tool

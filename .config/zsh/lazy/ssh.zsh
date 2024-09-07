0d0855e26eebf0be92273f972d6e7f8259c2e423
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-09-06 20:40:46 -0700

    Zsh: robust SSH socket checks

--- .config/zsh/lazy/ssh.zsh
+++ .config/zsh/lazy/ssh.zsh
@@ -1,5 +1,11 @@
 if [[ "$OSTYPE" == darwin* ]]; then
-    export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
-elif [[ "$OSTYPE" == linux* ]]; then
-    export SSH_AUTH_SOCK=~/.1password/agent.sock
+    local ssh_agent_socket=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+    if [[ -S $ssh_agent_socket ]]; then
+        export SSH_AUTH_SOCK=$ssh_agent_socket
+    fi
+elif [[ "$OSTYPE" == linux* && -S ~/.1password/agent.sock ]]; then
+    local ssh_agent_socket=~/.1password/agent.sock
+    if [[ -S $ssh_agent_socket ]]; then
+        export SSH_AUTH_SOCK=$ssh_agent_socket
+    fi
 fi

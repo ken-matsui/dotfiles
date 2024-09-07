8d911f24c1a12ead365b2b3188eaf4ce253a79d9
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-09-06 20:42:06 -0700

    Zsh: better gcloud dir check

--- .config/zsh/lazy/gcloud.zsh
+++ .config/zsh/lazy/gcloud.zsh
@@ -1,5 +1,9 @@
-if (( $+commands[brew] )) && [[ "$OSTYPE" == darwin* ]]; then
-  # Google Cloud SDK (gcloud command)
-  source $HOMEBREW_PREFIX/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
-  source $HOMEBREW_PREFIX/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc
+if [[ $+commands[brew] -eq 1 && "$OSTYPE" == darwin* ]]; then
+  local cloud_sdk_path="$HOMEBREW_PREFIX/Caskroom/google-cloud-sdk/latest/google-cloud-sdk"
+
+  if [[ -d "$cloud_sdk_path" ]]; then
+    # Google Cloud SDK (gcloud command)
+    source "$cloud_sdk_path/completion.zsh.inc"
+    source "$cloud_sdk_path/path.zsh.inc"
+  fi
 fi

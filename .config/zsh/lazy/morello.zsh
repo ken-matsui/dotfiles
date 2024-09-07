cd9fb9bbd8e66d51c85bc7283f32f78a40437cb5
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-09-06 20:36:21 -0700

    Zsh: removed morello

--- .config/zsh/lazy/morello.zsh
+++
@@ -1,6 +0,0 @@
-if [[ "$OSTYPE" == darwin* ]]; then
-  # export CLANG=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang
-  export CLANG=/opt/homebrew/opt/llvm/bin/clang
-else
-  export CLANG=clang
-fi

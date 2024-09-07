b7b4698231f35f69addc37beec7414f287a69aaa
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-09-06 17:36:00 -0700

    Zsh: simplify keymaps

--- .config/zsh/keymaps.zsh
+++ .config/zsh/keymaps.zsh
@@ -2,21 +2,5 @@
 # Key Bindings
 ##################################
 
-# Reset key binds
-bindkey -d
-# Remove default key binds
-for KEY in {A-Z}; do bindkey -r "^$KEY"; done
-# Restore some default key binds
-bindkey '^M' accept-line # for enter key
-bindkey '^I' expand-or-complete # for tab key
-bindkey '^U' kill-whole-line
-bindkey '^L' clear-screen
-# Restore some key binds with other keys
 bindkey -r '^['
 bindkey '^V' vi-cmd-mode # default is ^[
-# Enable bash like key binds
-bindkey '^R' history-incremental-search-backward
-bindkey '^A' beginning-of-line
-bindkey '^E' end-of-line
-bindkey '^P' up-line-or-search
-bindkey '^N' down-line-or-search

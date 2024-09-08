93a344fbf29ec88e9c673468dc0930974784a9e8
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-09-07 21:02:58 -0700

    Zsh: mojo only on macOS

--- .config/zsh/lazy/mojo.zsh
+++ .config/zsh/lazy/mojo.zsh
@@ -1,2 +1,4 @@
-export MODULAR_HOME="$HOME/.modular"
-export PATH="$HOME/.modular/pkg/packages.modular.com_mojo/bin:$PATH"
+if [[ "$OSTYPE" == darwin* ]]; then
+  export MODULAR_HOME="$HOME/.modular"
+  export PATH="$HOME/.modular/pkg/packages.modular.com_mojo/bin:$PATH"
+fi

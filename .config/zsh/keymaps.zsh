f3d03986d9217c4364c38a5bb378e1a028d0c8f5
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-09-25 18:06:33 -0400

    Zsh: restore bash-like key bindings

--- .config/zsh/keymaps.zsh
+++ .config/zsh/keymaps.zsh
@@ -4,3 +4,10 @@
 
 bindkey -r '^['
 bindkey '^V' vi-cmd-mode # default is ^[
+
+# Enable bash-like key bindings
+bindkey '^R' history-incremental-search-backward
+bindkey '^A' beginning-of-line
+bindkey '^E' end-of-line
+bindkey '^P' up-line-or-search
+bindkey '^N' down-line-or-search

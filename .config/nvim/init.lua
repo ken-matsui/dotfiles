d8b57f733c3e05f2c4accbffad7d37f639a924ff
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-09-30 18:24:53 -0400

    Nvim: format init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -23,12 +23,12 @@ vim.opt.signcolumn = "yes"
 
 -- Tmux integration (tmux-window-name)
 local uv = vim.loop
-vim.api.nvim_create_autocmd({ 'VimEnter', 'VimLeave' }, {
-  callback = function()
-    if vim.env.TMUX_PLUGIN_MANAGER_PATH then
-      uv.spawn(vim.env.TMUX_PLUGIN_MANAGER_PATH .. '/tmux-window-name/scripts/rename_session_windows.py', {})
-    end
-  end,
+vim.api.nvim_create_autocmd({ "VimEnter", "VimLeave" }, {
+	callback = function()
+		if vim.env.TMUX_PLUGIN_MANAGER_PATH then
+			uv.spawn(vim.env.TMUX_PLUGIN_MANAGER_PATH .. "/tmux-window-name/scripts/rename_session_windows.py", {})
+		end
+	end,
 })
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"

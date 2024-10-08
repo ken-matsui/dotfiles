b58d95115e65b1a98e33c3972da05185e09168f2
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-10-08 07:21:09 -0400

    Nvim: correctly use tmux-window-name

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -27,8 +27,8 @@ vim.opt.signcolumn = "yes"
 local uv = vim.loop
 vim.api.nvim_create_autocmd({ "VimEnter", "VimLeave" }, {
 	callback = function()
-		if vim.env.TMUX_PLUGIN_MANAGER_PATH then
-			uv.spawn(vim.env.TMUX_PLUGIN_MANAGER_PATH .. "/tmux-window-name/scripts/rename_session_windows.py", {})
+		if vim.env.TMUX_WINDOW_NAME_SCRIPT then
+			uv.spawn(vim.env.TMUX_WINDOW_NAME_SCRIPT, {})
 		end
 	end,
 })
--- .config/zsh/lazy/tmux.zsh
+++ .config/zsh/lazy/tmux.zsh
@@ -1,9 +1,12 @@
 if (( $+commands[tmux] )); then
   export TMUX_PLUGIN_PATH="$XDG_CONFIG_HOME/tmux/plugins"
 
-  tmux-window-name() {
-    ($TMUX_PLUGIN_PATH/tmux-window-name/scripts/rename_session_windows.py &)
-  }
+  if [[ -f "$TMUX_PLUGIN_PATH/tmux-window-name/scripts/rename_session_windows.py" ]]; then
+    export TMUX_WINDOW_NAME_SCRIPT="$TMUX_PLUGIN_PATH/tmux-window-name/scripts/rename_session_windows.py"
 
-  add-zsh-hook chpwd tmux-window-name
+    tmux-window-name() {
+      ($TMUX_WONDOW_NAME_SCRIPT &)
+    }
+    add-zsh-hook chpwd tmux-window-name
+  fi
 fi

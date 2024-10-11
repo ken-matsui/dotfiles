bb4b498fca6aa39d4fde7188babd9750338fd636
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-10-10 21:12:49 -0400

    Nvim: added trouble & edgy

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -47,11 +47,11 @@ return {
 			"nvim-tree/nvim-web-devicons",
 		},
 		keys = {
-			{ "<Leader>h", "<Cmd>BufferLineCyclePrev<Cr>", "n" },
-			{ "<Leader>l", "<Cmd>BufferLineCycleNext<Cr>", "n" },
-			{ "<Leader>bp", "<Cmd>BufferLinePick<Cr>", "n" },
-			{ "<Leader><Left>", "<Cmd>BufferLineMovePrev<Cr>", "n" },
-			{ "<Leader><Right>", "<Cmd>BufferLineMoveNext<Cr>", "n" },
+			{ "<Leader>h", "<Cmd>BufferLineCyclePrev<Cr>", "n", desc = "Go to left buffer" },
+			{ "<Leader>l", "<Cmd>BufferLineCycleNext<Cr>", "n", desc = "Go to right buffer" },
+			{ "<Leader>p", "<Cmd>BufferLinePick<Cr>", "n", desc = "Pick buffer" },
+			{ "<Leader><Left>", "<Cmd>BufferLineMovePrev<Cr>", "n", desc = "Move buffer to left" },
+			{ "<Leader><Right>", "<Cmd>BufferLineMoveNext<Cr>", "n", desc = "Move buffer to right" },
 		},
 		opts = {
 			options = {
@@ -76,6 +76,7 @@ return {
 					require("bufdelete").bufdelete(0, true)
 				end,
 				"n",
+				desc = "Close buffer",
 			},
 		},
 	},
@@ -106,6 +107,80 @@ return {
 			extensions = {
 				"nvim-tree",
 				"toggleterm",
+			},
+		},
+	},
+
+	{
+		"folke/trouble.nvim",
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
+	},
+
+	{
+		"folke/edgy.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.opt.laststatus = 3
+			vim.opt.splitkeep = "screen"
+		end,
+		opts = {
+			bottom = {
+				"Trouble",
+				{ ft = "qf", title = "QuickFix" },
+				{
+					ft = "help",
+					size = { height = 20 },
+					-- only show help buffers
+					filter = function(buf)
+						return vim.bo[buf].buftype == "help"
+					end,
+				},
+			},
+			left = {
+				{
+					title = "NvimTree",
+					ft = "NvimTree",
+					size = { height = 0.5 },
+				},
+				{
+					title = "Outline",
+					ft = "Outline",
+					pinned = true,
+					open = "SymbolsOutlineOpen",
+				},
 			},
 		},
 	},

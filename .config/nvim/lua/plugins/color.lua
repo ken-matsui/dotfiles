74a633bf1d6a07f56bfa9ade3c98c04e08ff1b9d
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-10-11 23:45:27 -0400

    Nvim, Vim: remove Shade.nvim & use simple forcus handling

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -132,15 +132,4 @@ return {
 			})
 		end,
 	},
-
-	-- Dim inactive window
-	{
-		"sunjon/Shade.nvim",
-		lazy = true,
-		event = "VeryLazy",
-		opts = {
-			overlay_opacity = 50,
-			opacity_step = 1,
-		},
-	},
 }

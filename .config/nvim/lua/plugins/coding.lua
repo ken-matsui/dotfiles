4160f968ec3b3538fe9cbaf3b66bb3ec7eec08d2
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-10-12 11:21:56 -0400

    Nvim: move basic.lua into coding.lua

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -1,4 +1,23 @@
 return {
+	-- Detect shiftwidth and expandtab automatically
+	"tpope/vim-sleuth",
+
+	"dkarter/bullets.vim",
+
+	"farmergreg/vim-lastplace",
+
+	{
+		"tpope/vim-repeat",
+		lazy = true,
+		event = "VeryLazy",
+	},
+
+	{
+		"machakann/vim-sandwich",
+		lazy = true,
+		event = "VeryLazy",
+	},
+
 	{
 		"github/copilot.vim", -- :Copilot setup
 		lazy = true,

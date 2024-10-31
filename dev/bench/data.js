74b88f7a04b2f343d98ed50f049313d2bd213a8b
github-action-benchmark <github@users.noreply.github.com>
2024-10-31 23:13:12 +0000

    add Neovim Benchmark (customSmallerIsBetter) benchmark result for 203633e575fd6d5305b7a42a0e5232ab847c2540

---
+++ dev/bench/data.js
@@ -0,0 +1,33 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1730416392357,
+  "repoUrl": "https://github.com/ken-matsui/dotfiles",
+  "entries": {
+    "Neovim Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "ken-matsui",
+            "username": "ken-matsui"
+          },
+          "committer": {
+            "name": "ken-matsui",
+            "username": "ken-matsui"
+          },
+          "id": "203633e575fd6d5305b7a42a0e5232ab847c2540",
+          "message": "Nvim: continuous benchmarks",
+          "timestamp": "2024-10-31T21:59:10Z",
+          "url": "https://github.com/ken-matsui/dotfiles/pull/64/commits/203633e575fd6d5305b7a42a0e5232ab847c2540"
+        },
+        "date": 1730416391646,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Neovim Total Average Startup Time",
+            "value": 8.0061,
+            "unit": "msec"
+          }
+        ]
+      }
+    ]
+  }
+}

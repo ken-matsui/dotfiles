776eee31963173db7680ce6c731585803321f823
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-11-18 18:27:57 -0500

    Shell: some shellcheck fixes

--- .config/shell/handlers.zsh
+++ .config/shell/handlers.zsh
@@ -1,7 +1,7 @@
 command_not_found_handler() {
   if command_exists suggest-command-not-found; then
-    exec suggest-command-not-found $@
+    exec suggest-command-not-found "$@"
   else
-    echo "zsh: command not found: $@"
+    echo "zsh: command not found: $*"
   fi
 }

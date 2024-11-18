8e3d8f93029870e573eb43cc4c1c679f63909999
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-11-18 13:23:17 -0500

    Zsh: use command_exists for handlers

--- .config/zsh/handlers.sh
+++ .config/zsh/handlers.sh
@@ -1,6 +1,6 @@
 # For Zsh
 command_not_found_handler() {
-  if command -v suggest-command-not-found &> /dev/null; then
+  if command_exists suggest-command-not-found; then
     exec suggest-command-not-found $@
   else
     echo "zsh: command not found: $@"
@@ -9,7 +9,7 @@ command_not_found_handler() {
 
 # For Bash
 command_not_found_handle() {
-  if command -v suggest-command-not-found &> /dev/null; then
+  if command_exists suggest-command-not-found; then
     exec suggest-command-not-found $@
   else
     echo "bash: command not found: $@"

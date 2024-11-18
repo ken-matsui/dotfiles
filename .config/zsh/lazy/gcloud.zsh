8622eb6fe95755074d0b2d48a8c01d11e61d829d
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-11-18 13:01:56 -0500

    Zsh: remove gcloud.zsh

--- .config/zsh/lazy/gcloud.zsh
+++
@@ -1,9 +0,0 @@
-if [[ $+commands[brew] -eq 1 && "$OSTYPE" == darwin* ]]; then
-  local cloud_sdk_path="$HOMEBREW_PREFIX/Caskroom/google-cloud-sdk/latest/google-cloud-sdk"
-
-  if [[ -d "$cloud_sdk_path" ]]; then
-    # Google Cloud SDK (gcloud command)
-    source "$cloud_sdk_path/completion.zsh.inc"
-    source "$cloud_sdk_path/path.zsh.inc"
-  fi
-fi

174f16094f8cdc94909bf3c4ad0edfe7281437ab
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-11-18 13:27:55 -0500

    Zsh: use uname instead of OSTYPE

--- .config/zsh/lazy/ssh.sh
+++ .config/zsh/lazy/ssh.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
-case "$OSTYPE" in
-    darwin*)
+case "$(uname)" in
+    Darwin)
         _ssh_agent_socket="$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
         ;;
-    linux*)
+    Linux)
         _ssh_agent_socket="$HOME/.1password/agent.sock"
         ;;
     *)

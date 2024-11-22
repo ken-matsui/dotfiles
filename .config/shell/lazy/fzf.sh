6a9526e5ac814f02aa4eea0cab96f8dff99bb6fe
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-11-22 09:44:16 -0500

    Shell: fix load condition for fzf

--- .config/shell/lazy/fzf.sh
+++ .config/shell/lazy/fzf.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-[ -f "$HOME"/.fzf.zsh ] && . "$HOME"/.fzf.zsh
-[ -f "$HOME"/.fzf.bash ] && . "$HOME"/.fzf.bash
+is_zsh && [ -f "$HOME"/.fzf.zsh ] && . "$HOME"/.fzf.zsh
+is_bash && [ -f "$HOME"/.fzf.bash ] && . "$HOME"/.fzf.bash
 
 if command_exists fzf; then
   export FZF_DEFAULT_OPTS="--height 12"

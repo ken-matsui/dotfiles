03febe37a2b43d1aee42def68c0e0635f4e8a1e7
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-11-27 00:52:41 -0500

    Zinit: style tweak

--- .config/shell/zinit.zsh
+++ .config/shell/zinit.zsh
@@ -6,8 +6,8 @@ declare -A ZINIT
 ZINIT[NO_ALIASES]=1
 
 ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
-[ ! -d $ZINIT_HOME ] && mkdir -p "$(dirname $ZINIT_HOME)"
-[ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+[ ! -d "$ZINIT_HOME" ] && mkdir -p "$(dirname "$ZINIT_HOME")"
+[ ! -d "$ZINIT_HOME/.git" ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
 source "${ZINIT_HOME}/zinit.zsh"
 
 zinit snippet PZTM::directory

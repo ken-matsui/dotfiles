9d3cd7727d06e8e679382e339f5acf522f57f679
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-11-27 00:59:35 -0500

    Zinit: XDG_DATA_HOME should always be available

--- .config/shell/zinit.zsh
+++ .config/shell/zinit.zsh
@@ -6,7 +6,7 @@ declare -A ZINIT
 ZINIT[PLUGINS_DIR]="$SHELL_CONFIG_HOME/plugins"
 ZINIT[NO_ALIASES]=1
 
-ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
+ZINIT_HOME="$XDG_DATA_HOME/zinit/zinit.git"
 [ ! -d "$ZINIT_HOME" ] && mkdir -p "$(dirname "$ZINIT_HOME")"
 [ ! -d "$ZINIT_HOME/.git" ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
 source "${ZINIT_HOME}/zinit.zsh"

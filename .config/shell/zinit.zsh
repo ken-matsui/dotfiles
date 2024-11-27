4b21047b6ad3dc2612b0625902430a6b26fd6b5b
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-11-27 00:46:37 -0500

    Zinit: disable aliases

--- .config/shell/zinit.zsh
+++ .config/shell/zinit.zsh
@@ -2,6 +2,9 @@
 # Zinit Options & Plugins
 ##################################
 
+declare -A ZINIT
+ZINIT[NO_ALIASES]=1
+
 ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 [ ! -d $ZINIT_HOME ] && mkdir -p "$(dirname $ZINIT_HOME)"
 [ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"

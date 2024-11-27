db6eeae1b56fab5478802a7c8482df95b8b3e9d5
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-11-27 00:38:24 -0500

    Nvim: update lazy config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -53,23 +53,7 @@ vim.api.nvim_set_keymap(
 	{ silent = true, noremap = true, desc = "Close All Sub-windows" }
 )
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins", {
-	change_detection = {
-		notify = false,
-	},
-})
+require("config.lazy")
 
 -- Must be written at the last.  see :help 'secure'.
 vim.opt.secure = true

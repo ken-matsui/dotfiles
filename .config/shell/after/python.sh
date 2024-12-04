81268d996531749be0394fcb80f52542dd99a495
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-12-03 20:20:35 -0500

    Zsh: use python@3

--- .config/shell/after/python.sh
+++ .config/shell/after/python.sh
@@ -2,5 +2,5 @@
 
 if command_exists brew; then
   export PATH="$HOMEBREW_PREFIX/bin:$PATH"
-  export PATH="$HOMEBREW_PREFIX/opt/python@3.10/libexec/bin:$PATH"
+  export PATH="$(brew --prefix python@3)/libexec/bin:$PATH"
 fi

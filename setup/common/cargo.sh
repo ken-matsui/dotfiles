fe03e6d425ce6c9b0097319f57346af6a00b6ee1
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-12-18 20:00:58 -0500

    Psync: removed as unnecessary

--- setup/common/cargo.sh
+++ setup/common/cargo.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 set -eu
 
-cargo install cargo-update cargo-cache suggest-command-not-found git-interactive-rebase-tool
+cargo install \
+  cargo-update \
+  cargo-cache \
+  suggest-command-not-found \
+  git-interactive-rebase-tool

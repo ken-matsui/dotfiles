31ecce438e8c4172231491883941de84b076e82a
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2025-02-06 20:48:51 -0500

    Nvim: settings for lua_ls

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -287,7 +287,33 @@ return {
 				lspconfig.clangd.setup({})
 			end
 			if vim.fn.executable("lua-language-server") == 1 then
-				lspconfig.lua_ls.setup({})
+				lspconfig.lua_ls.setup({
+					-- https://github.com/neovim/neovim/issues/21686#issuecomment-1522446128
+					settings = {
+						Lua = {
+							runtime = {
+								-- Tell the language server which version of Lua you're using
+								-- (most likely LuaJIT in the case of Neovim)
+								version = "LuaJIT",
+							},
+							diagnostics = {
+								-- Get the language server to recognize the `vim` global
+								globals = {
+									"vim",
+									"require",
+								},
+							},
+							workspace = {
+								-- Make the server aware of Neovim runtime files
+								library = vim.api.nvim_get_runtime_file("", true),
+							},
+							-- Do not send telemetry data containing a randomized but unique identifier
+							telemetry = {
+								enable = false,
+							},
+						},
+					},
+				})
 			end
 			if vim.fn.executable("rust-analyzer") == 1 then
 				lspconfig.rust_analyzer.setup({})

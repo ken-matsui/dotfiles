2ce6591cab7c2887ec3c52ba552ceaec7ea70706
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2025-03-22 10:14:21 -0400

    nvim: remove lsp_signature

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -340,14 +340,4 @@ return {
 			end
 		end,
 	},
-
-	{
-		"ray-x/lsp_signature.nvim",
-		version = "v0.3.*",
-		event = "VeryLazy",
-		opts = {},
-		config = function(_, opts)
-			require("lsp_signature").setup(opts)
-		end,
-	},
 }

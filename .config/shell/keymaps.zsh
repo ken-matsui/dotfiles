3758ecf7d9f1e451dcb3a9b5b9b8783d00d66da8
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2025-08-27 21:50:18 -0400

    zsh: add more key-bindings from ohmyzsh

--- .config/shell/keymaps.zsh
+++ .config/shell/keymaps.zsh
@@ -2,6 +2,15 @@
 bindkey -e
 # NOTE: '^X^V' vi-cmd-mode
 
+# [Ctrl-RightArrow] - move forward one word
+bindkey '^[[1;5C' forward-word
+# [Ctrl-LeftArrow] - move backward one word
+bindkey '^[[1;5D' backward-word
+
+# Edit the current command line in $VISUAL (or $EDITOR / `vi` if not set)
 autoload -z edit-command-line
 zle -N edit-command-line
 bindkey '^X^E' edit-command-line
+
+# File rename magick (Alt+m or Esc m)
+bindkey '^[m' copy-prev-shell-word

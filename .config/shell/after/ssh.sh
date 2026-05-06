1c228f3d3e3d329d22a024e40c1f504ae90bca19
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2026-05-05 20:22:52 -0400

    sh: simplify ssh.sh

--- .config/shell/after/ssh.sh
+++ .config/shell/after/ssh.sh
@@ -4,8 +4,4 @@ if ! command_exists pass-cli; then
   return
 fi
 
-_pp_sock="$HOME/.ssh/proton-pass-agent.sock"
-export SSH_AUTH_SOCK="$_pp_sock"
-# if [ -S "$_pp_sock" ]; then
-#     export SSH_AUTH_SOCK="$_pp_sock"
-# fi
+export SSH_AUTH_SOCK="$HOME/.ssh/proton-pass-agent.sock"

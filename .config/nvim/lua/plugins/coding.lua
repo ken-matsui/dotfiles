873f7f6d0bb799cc1722568c552cabc663b8ebb1
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2026-09-18 02:50:33 -0400

    nvim: remove unnecessary opts for gitsigns

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -151,9 +151,6 @@ return {
 			signs = {
 				add = { text = "+" },
 				change = { text = "~" },
-				delete = { text = "_" },
-				topdelete = { text = "‾" },
-				changedelete = { text = "~" },
 			},
 			on_attach = function(bufnr)
 				local gitsigns = require("gitsigns")
